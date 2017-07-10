e5dcc976e61a12c8decd45adfacb0a7d90029ab3
Johannes Buchner <johannes.buchner.acad@gmx.com>
2017-07-10 14:53:15 +0200

    updated draft to horns example

---
+++ gensimple_horns.py
@@ -0,0 +1,68 @@
+import numpy
+import matplotlib.pyplot as plt
+import h5py
+from numpy import exp, pi, arctan
+import sys
+
+def gauss(x, A, mu, sig):
+	xT = x.reshape((1,-1))
+	AT = A.reshape((-1,1))
+	muT = mu.reshape((-1,1))
+	sigT = sig.reshape((-1,1))
+	return AT * exp(-0.5 * ((muT - xT)/sigT)**2)
+
+x = numpy.linspace(400, 800, 200)
+
+N = 40
+N = int(sys.argv[1])
+numpy.random.seed(N)
+z = arctan(numpy.random.uniform(-pi, pi, size=N)) * 0.1
+rest_wave = 656
+print 'generating parameters ...'
+width_narrow = 5.0 * numpy.ones(N)
+mean_narrow = rest_wave * (1 + z)
+width_narrow = width_narrow
+noise_level = 0.01
+signal_level = 0.02 / numpy.random.power(3, size=N)
+height_narrow = signal_level
+
+print 'generating signal ...'
+ym = gauss(A=height_narrow, mu=mean_narrow, x=x, sig=width_narrow)
+ym = numpy.transpose(ym)
+print ym.shape
+
+# add noise
+print 'adding noise...'
+y = ym.copy()
+for i in range(N):
+	y[:,i] += numpy.random.normal(0, noise_level, size=len(x))
+
+print 'plotting ...'
+#for i in range(min(N, 20)):
+#	#plt.plot(x, y[:,i], '.-')
+#	plt.plot(x, y[:,i], '-')
+#plt.savefig('gen_widths.pdf', bbox_inches='tight')
+#plt.close()
+colors = ['yellow', 'pink', 'cyan', 'magenta']
+colors = ['magenta', 'cyan', 'pink', 'yellow']
+for i in range(min(N, 4)):
+	#plt.plot(x, y[:,i], '.-')
+	plt.plot(rest_wave * (1 + z[i]), 1.1 * y[:,i].max() / noise_level, 'v', color=colors[i], ms=12)
+	#plt.plot(rest_wave * (1 + z[i]), 4, 'v', color=colors[i], ms=12)
+	plt.plot(x, y[:,i] / noise_level, '-', color=colors[i])
+plt.ylabel('Detector signal')
+plt.xlabel('Wavelength [nm]')
+plt.savefig('genhorns.pdf', bbox_inches='tight')
+plt.close()
+
+
+#print x.shape, y.shape, z.shape
+with h5py.File('data_widths_%s.hdf5' % sys.argv[1], 'w') as f:
+	f.create_dataset('x', data=x, compression='gzip', shuffle=True)
+	f.create_dataset('y', data=y, compression='gzip', shuffle=True)
+	f.create_dataset('z', data=z, compression='gzip', shuffle=True)
+	f.create_dataset('mean_narrow', data=mean_narrow, compression='gzip', shuffle=True)
+	f.create_dataset('width_narrow', data=width_narrow, compression='gzip', shuffle=True)
+	f.create_dataset('height_narrow', data=height_narrow, compression='gzip', shuffle=True)
+
+
--- sample.py
+++ sample.py
@@ -7,55 +7,37 @@ import os
 import time
 
 print 'loading data...'
-f = h5py.File(sys.argv[1], 'r')
-x = f['x'].value
-y = f['y'].value
+ndata = int(sys.argv[2])
+with h5py.File(sys.argv[1], 'r') as f:
+	x = numpy.array(f['x'].value)
+	y = numpy.array(f['y'][:,:ndata])
 
 def gauss(x, z, A, mu, sig):
 	return A * exp(-0.5 * ((mu - x / (1. + z))/sig)**2)
 
 nx, ndata = y.shape
-rest_wave = 440
 noise_level = 0.01
 params = ['A', 'mu', 'sig'] #, 'noise_level']
 nparams = len(params)
 def priortransform(cube):
 	cube = cube.copy()
 	cube[0] = 10**(cube[0] * 2 - 2)
-	#cube[0] = cube[0] * 10
 	cube[1] = cube[1] * 400 + 400
-	cube[2] = cube[2] * 2 + 2
-	##cube[3] = 10**(cube[3] * 2 - 4)
+	cube[2] = cube[2] * 2
 	return cube
-
-#def priortransform(cube):
-#	cube = cube.copy()
-#	cube[0] = 10**(cube[0] * 10 - 5)
-#	cube[1] = cube[1] * 400 + 400
-#	cube[2] = cube[0] * 10 - 5
-#	return cube
 
 def model(params):
 	A, mu, log_sig_kms = params
-	sig = 10**log_sig_kms * rest_wave / 300000
+	sig = 10**log_sig_kms
 	z = 0
 	ypred = gauss(x, z, A, mu, sig)
 	return ypred
 
 def multi_loglikelihood(params, data_mask):
-	#print 'Like: model'
-	
-	#ypred = model(params)
-	# inlining for speed-up
 	A, mu, log_sig_kms = params
-	sig = 10**log_sig_kms * rest_wave / 300000
+	sig = 10**log_sig_kms
 	ypred = A * exp(-0.5 * ((mu - x)/sig)**2)
-	# end of inlining
-	
-	#print 'Like: data-model'
 	L = -0.5 * (((ypred.reshape((-1,1)) - y[:,data_mask])/noise_level)**2).sum(axis=0)
-	#print 'Like done'
-	#assert L.shape == (data_mask.sum(),), (L.shape, ndata)
 	return L
 
 #print multi_loglikelihood([0.88091237,  444.44207558,    2.77671952], numpy.ones(ndata)==1)
@@ -84,7 +66,7 @@ lib.like.argtypes = [
 
 def multi_loglikelihood(params, data_mask):
 	A, mu, log_sig_kms = params
-	sig = 10**log_sig_kms * rest_wave / 300000
+	sig = 10**log_sig_kms
 	Lout = numpy.zeros(data_mask.sum())
 	ret = lib.like(x, y, ndata, nx, A, mu, sig, noise_level, data_mask, Lout)
 	#assert ret == 0, (ret, -0.5*Lout)
@@ -103,17 +85,66 @@ numpy.random.seed(1)
 start_time = time.time()
 print 'setting up integrator ...'
 nlive_points = int(os.environ.get('NLIVE_POINTS','400'))
+
+# constrained region draw functions
+# we try hard to keep information about current regions and subselected regions
+# because recomputing the regions is expensive if the likelihood is very fast.
+# There are three constrainers:
+#   - the one of the superset (all data sets)
+#   - one for each data set if need a individual draw (focussed draw with only one)
+#   - a memory for recent clusterings, because they might recur in the next iteration(s)
+# Note that this does caching not improve the algorithms efficiency
+#   in fact, not recomputing regions keeps the regions larger, 
+#   leading potentially to slightly more rejections. 
+# However, there is substantial execution speedup.
+
 superset_constrainer = MetricLearningFriendsConstrainer(metriclearner = 'truncatedscaling', 
 	rebuild_every=20, metric_rebuild_every=20, verbose=False, force_shrink=True)
-focusset_constrainer = MetricLearningFriendsConstrainer(metriclearner = 'truncatedscaling', 
-	rebuild_every=1, metric_rebuild_every=1, verbose=False)
+class CachedConstrainer(object):
+	"""
+	This keeps metric learners if they are used (in the last three iterations).
+	Otherwise, constructs a fresh one.
+	"""
+	def __init__(self):
+		self.iter = None
+		self.prev_prev_prev_generation = {}
+		self.prev_prev_generation = {}
+		self.prev_generation = {}
+		self.curr_generation = {}
+	def get(self, mask, it):
+		k = tuple(mask.tolist())
+		if it != self.iter:
+			# new generation
+			self.prev_prev_prev_generation = self.prev_prev_generation
+			self.prev_prev_generation = self.prev_generation
+			self.prev_generation = self.curr_generation
+			self.curr_generation = {}
+		
+		if k in self.curr_generation:
+			pass
+		elif k in self.prev_generation:
+			self.curr_generation[k] = self.prev_generation[k]
+		elif k in self.prev_prev_generation:
+			self.curr_generation[k] = self.prev_prev_generation[k]
+		elif k in self.prev_prev_prev_generation:
+			self.curr_generation[k] = self.prev_prev_prev_generation[k]
+		else:
+			self.curr_generation[k] = MetricLearningFriendsConstrainer(
+				metriclearner = 'truncatedscaling', force_shrink=True,
+				rebuild_every=20, metric_rebuild_every=20, 
+				verbose=False)
+			self.curr_generation[k].sampler = sampler
+		
+		return self.curr_generation[k].draw_constrained
+
+focusset_constrainer = CachedConstrainer().get
 individual_constrainers = {}
 individual_constrainers_lastiter = {}
 def individual_draw_constrained(i, it):
 	if i not in individual_constrainers:
 		individual_constrainers[i] = MetricLearningFriendsConstrainer(
 			metriclearner = 'truncatedscaling', force_shrink=True,
-			rebuild_every=5, metric_rebuild_every=5, 
+			rebuild_every=20, metric_rebuild_every=20, 
 			verbose=False)
 		individual_constrainers[i].sampler = sampler
 		individual_constrainers_lastiter[i] = it
@@ -128,18 +159,17 @@ sampler = MultiNestedSampler(nlive_points = nlive_points,
 	ndim=nparams, ndata=ndata,
 	superset_draw_constrained = superset_constrainer.draw_constrained, 
 	individual_draw_constrained = individual_draw_constrained,
-	draw_constrained = focusset_constrainer.draw_constrained, 
+	draw_constrained = focusset_constrainer, 
 	nsuperset_draws = int(os.environ.get('SUPERSET_DRAWS', '10')),
 	use_graph = os.environ.get('USE_GRAPH', '1') == '1'
 	)
-focusset_constrainer.sampler = sampler
 superset_constrainer.sampler = sampler
 print 'integrating ...'
 results = multi_nested_integrator(tolerance=0.5, multi_sampler=sampler, min_samples=0) #, max_samples=1000)
 duration = time.time() - start_time
 print 'writing output files ...'
 # store results
-with h5py.File(sys.argv[1] + '.out.hdf5', 'w') as f:
+with h5py.File(sys.argv[1] + '.out4.hdf5', 'w') as f:
 	f.create_dataset('logZ', data=results['logZ'], compression='gzip', shuffle=True)
 	f.create_dataset('logZerr', data=results['logZerr'], compression='gzip', shuffle=True)
 	u, x, L, w, mask = zip(*results['weights'])
@@ -150,11 +180,11 @@ with h5py.File(sys.argv[1] + '.out.hdf5', 'w') as f:
 	f.create_dataset('mask', data=mask, compression='gzip', shuffle=True)
 	f.create_dataset('ndraws', data=sampler.ndraws)
 	print 'logZ = %.1f +- %.1f' % (results['logZ'][0], results['logZerr'][0])
-	print 'ndraws:', sampler.ndraws
+	print 'ndraws:', sampler.ndraws, 'niter:', len(w)
 
 print 'writing statistic ...'
-json.dump(dict(ndraws=sampler.ndraws, duration=duration), 
-	open(sys.argv[1] + '.out.stats.json', 'w'))
+json.dump(dict(ndraws=sampler.ndraws, duration=duration, ndata=ndata, niter=len(w)), 
+	open(sys.argv[1] + '_%d.out4cum.stats.json' % ndata, 'w'), indent=4)
 print 'done.'
 
 
